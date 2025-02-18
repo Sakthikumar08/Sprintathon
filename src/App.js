59ded22fe944d70d51bb8b3a944401b10e051995
Sakthikumar08 <sakthikumarcricket@gmail.com>
2025-02-18 11:31:06 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
 import Home from './components/Home';
 import Navbar from "./components/header/Navbar";
 import Domain from './components/Domain';
+import Prize from "./components/Prize";
+import Rules from "./components/Rules";
+import Sponsors from "./components/Sponsors";
 import Contact from './components/Contact';
 
 function App() {
   return (
-   <>
- 
- <Navbar />
-            <div id="home" style={{ height: '105vh',}}>
-            <Home />
-            </div>
-            <div id="Domain" style={{ height: '105vh',}}>
-            <Domain />
-            </div>
-            <div id="contact" style={{ height: '105vh',}}>
-            <Contact />
-            </div>
-             
-   </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/track" element={<Domain />} />
+        <Route path="/prize" element={<Prize />} />
+        <Route path="/sponsors" element={<Sponsors />} />
+        <Route path="/rules" element={<Rules />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,49 +1,34 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./NavbarStyle.css";
-import { Link } from "react-scroll";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <header className="navbar">
- {/*      <div className="brand">
-        <h1>
-          <span>INN</span>VENIOX
-        </h1>
-      </div>  */}
-      
       <nav className={`nav-list ${menuOpen ? "active" : ""}`}>
         <ul>
           <li>
-            <Link to="home" spy={true} smooth={true} offset={-70} duration={500} onClick={() => setMenuOpen(false)}>
-              Home
-            </Link>
+            <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
           </li>
           <li>
-            <Link to="about" spy={true} smooth={true} offset={-70} duration={500} onClick={() => setMenuOpen(false)}>
-              Domain
-            </Link>
+            <Link to="/domain" onClick={() => setMenuOpen(false)}>Domain</Link>
           </li>
           <li>
-            <Link to="projects" spy={true} smooth={true} offset={-70} duration={500} onClick={() => setMenuOpen(false)}>
-              Sponsors
-            </Link>
+            <Link to="/prize" onClick={() => setMenuOpen(false)}>Prize</Link>
           </li>
           <li>
-            <Link to="contact" spy={true} smooth={true} offset={-70} duration={500} onClick={() => setMenuOpen(false)}>
-              Contact
-            </Link>
+            <Link to="/sponsors" onClick={() => setMenuOpen(false)}>Sponsors</Link>
           </li>
           <li>
-            <Link to="contact" spy={true} smooth={true} offset={-70} duration={500} onClick={() => setMenuOpen(false)}>
-              Rules
-            </Link>
+            <Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
+          </li>
+          <li>
+            <Link to="/rules" onClick={() => setMenuOpen(false)}>Rules</Link>
           </li>
         </ul>
       </nav>
-
-    {/*   <button className="btn">Register</button> */}
 
       <div className={`hamburger ${menuOpen ? "active" : ""}`} onClick={() => setMenuOpen(!menuOpen)}>
         <span className="bar"></span>
